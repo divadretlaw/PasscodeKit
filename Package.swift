ee98a20645102a0d12ac86eb3d71d5b90be4e46a
David Walter <hello@davidwalter.at>
2024-03-26 23:43:24 +0100

    Update dependencies

--- Package.swift
+++ Package.swift
@@ -1,11 +1,12 @@
-// swift-tools-version: 5.6
+// swift-tools-version: 5.9
 
 import PackageDescription
 
 let package = Package(
     name: "PasscodeKit",
     platforms: [
-        .iOS(.v15)
+        .iOS(.v15),
+        .visionOS(.v1)
     ],
     products: [
         .library(
@@ -18,9 +19,9 @@ let package = Package(
         )
     ],
     dependencies: [
-        .package(url: "https://github.com/divadretlaw/WindowSceneReader.git", from: "2.1.0"),
-        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "20.0.0"),
-        .package(url: "https://github.com/siteline/swiftui-introspect", from: "1.0.0")
+        .package(url: "https://github.com/divadretlaw/WindowSceneReader.git", from: "3.0.0"),
+        .package(url: "https://github.com/evgenyneu/keychain-swift.git", from: "21.0.0"),
+        .package(url: "https://github.com/siteline/swiftui-introspect.git", from: "1.0.0")
     ],
     targets: [
         .target(
